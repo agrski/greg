f258f8038910ba0c7c270e44553aba387d4cafa0
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2022-01-08 17:42:05 +0000

    Rewrite URL-parsing function
    Rather than relying on url.Parse, which requires a scheme for correct parsing,
    this approach is more permissive.

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -65,22 +65,41 @@ func getLocation() (location, error) {
 }
 
 func parseLocationFromURL(rawURL string) (location, error) {
-	u, err := url.Parse(rawURL)
-	if err != nil {
-		return location{}, fmt.Errorf("unable to parse URL - %v", err)
+	if isEmpty(rawURL) {
+		return location{}, errors.New("cannot parse empty string")
 	}
 
-	p := u.Path
-	p = strings.TrimSuffix(p, ".git")
-	p = strings.TrimPrefix(p, "/")
-	orgAndRepo := strings.SplitN(p, "/", 3)
-	if len(orgAndRepo) < 2 || isEmpty(orgAndRepo[0]) || isEmpty(orgAndRepo[1]) {
-		return location{}, fmt.Errorf("unable to extract both org and repo from %s", u)
+	noWhitespace := strings.TrimSpace(rawURL)
+
+	parts := strings.SplitAfter(noWhitespace, "://")
+	if len(parts) > 2 {
+		return location{}, fmt.Errorf("cannot parse malformed string '%v'", noWhitespace)
+	}
+
+	withoutScheme := parts[len(parts)-1]
+	hostAndPath := strings.Split(withoutScheme, "/")
+	if len(hostAndPath) < 3 {
+		return location{}, fmt.Errorf("unable to parse host, org, and repo from %v", hostAndPath)
+	}
+
+	host := hostAndPath[0]
+	org := hostAndPath[1]
+	repo := hostAndPath[2]
+	repo = strings.TrimSuffix(repo, ".git")
+
+	if !strings.HasSuffix(host, "github.com") {
+		return location{}, fmt.Errorf("require github.com not %v", host)
+	}
+	if isEmpty(org) {
+		return location{}, errors.New("org cannot be empty")
+	}
+	if isEmpty(repo) {
+		return location{}, errors.New("repo cannot be empty")
 	}
 
 	return location{
-		organisationName(orgAndRepo[0]),
-		repositoryName(orgAndRepo[1]),
+		organisationName(org),
+		repositoryName(repo),
 	}, nil
 }
 

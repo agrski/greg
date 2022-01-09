c8c6c475601d0ee299439fdbdcf6c39ce6a6f0d4
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2022-01-09 01:07:17 +0000

    Add skeleton for fetcher package

--- pkg/fetch/fetch.go
+++ pkg/fetch/fetch.go
@@ -1,1 +1,27 @@
 package fetch
+
+/*
+	Strategy pattern that select from supported providers.
+	Currently the CLI knows about supported git providers,
+	but really that information should come from this package.
+*/
+
+type HostName string
+type OrganisationName string
+type RepositoryName string
+
+type Location struct {
+	Host         HostName
+	Organisation OrganisationName
+	Repository   RepositoryName
+}
+
+type Fetcher interface {
+	Start() error
+	Stop() error
+	Next() *interface{}
+}
+
+func NewFetcher(l Location) *Fetcher {
+	return nil
+}

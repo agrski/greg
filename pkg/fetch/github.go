2ff841af360a9ee0afaf8e3686faf038cba3a5a4
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2022-01-09 01:25:02 +0000

    Add placeholder file for GitHub (GraphQL) fetcher
    Add notes on potential GraphQL client libraries.

---
+++ pkg/fetch/github.go
@@ -0,0 +1,12 @@
+package fetch
+
+/*
+	Candidate GraphQL clients:
+	* https://github.com/hasura/go-graphql-client
+		- Generic usage (not GitHub-specific)
+		- Struct-based rather than string-based
+	* https://github.com/shurcooL/githubv4
+		- GitHub-specific
+		- Struct-based
+		- Supports pagination
+*/

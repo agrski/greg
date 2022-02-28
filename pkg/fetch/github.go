1f814092bb80dc43c068cdddc283a7ea1e13a6ff
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2022-02-28 00:28:46 +0000

    Add types & structue for basic GitHub API query
    The query itself is too basic to be efficient, but allows testing of the
    syntax and struct tags whilst learning the GraphQL library.
    It will be refactored and improved.

--- pkg/fetch/github.go
+++ pkg/fetch/github.go
@@ -1,5 +1,13 @@
 package fetch
 
+import (
+	"context"
+	"time"
+
+	"github.com/hasura/go-graphql-client"
+	"golang.org/x/oauth2"
+)
+
 /*
 	Candidate GraphQL clients:
 	* https://github.com/hasura/go-graphql-client
@@ -10,3 +18,74 @@ package fetch
 		- Struct-based
 		- Supports pagination
 */
+
+const (
+	apiUrl              = "https://api.github.com/graphql"
+	defaultQueryTimeout = 30 * time.Second
+)
+
+const (
+	TreeEntryDir  = "tree"
+	TreeEntryFile = "blob"
+)
+
+type gitHub struct {
+	client *graphql.Client
+}
+
+type queryParams struct {
+	repoOwner  string
+	repoName   string
+	commitish  string
+	pathPrefix string
+}
+
+func NewGitHub(accessToken string) *gitHub {
+	// TODO - refactor OAuth handling entirely outside this package
+	tokenSource := oauth2.StaticTokenSource(
+		&oauth2.Token{
+			AccessToken: accessToken,
+		},
+	)
+
+	authClient := oauth2.NewClient(context.Background(), tokenSource)
+	client := graphql.NewClient(apiUrl, authClient)
+
+	return &gitHub{
+		client: client,
+	}
+}
+
+func (g *gitHub) makeBaseQuery(params queryParams) (*Query, error) {
+	query := &Query{}
+
+	variables := map[string]interface{}{
+		"owner":            graphql.String(params.repoOwner),
+		"repo":             graphql.String(params.repoName),
+		"commitishAndPath": graphql.String(params.commitish + ":" + params.pathPrefix),
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), defaultQueryTimeout)
+	err := g.client.Query(ctx, query, variables)
+
+	return query, err
+}
+
+type Query struct {
+	Repository struct {
+		Name   string
+		Object struct {
+			FileInfoFragment `graphql:"... on Tree"`
+		} `graphql:"object(expression: $commitishAndPath)"`
+	} `graphql:"repository(owner: $owner, name: $repo)"`
+}
+
+type FileInfoFragment struct {
+	Entries []FileInfo
+}
+
+type FileInfo struct {
+	Name      string
+	Type      string
+	Extension string
+}

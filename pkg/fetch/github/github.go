6ac53e0b0d15a6345516f9c511751370594481a7
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2022-03-01 23:57:06 +0000

    Add blank lines for legibility/logical division of sections

--- pkg/fetch/github/github.go
+++ pkg/fetch/github/github.go
@@ -51,10 +51,12 @@ func (g *GitHub) getDefaultBranchRef(owner string, repo string) (string, error) 
 		"repo":  graphql.String(repo),
 	}
 	ctx, _ := context.WithTimeout(context.Background(), defaultQueryTimeout)
+
 	err := g.client.Query(ctx, q, variables)
 	if err != nil {
 		return "", err
 	}
+
 	return q.Repository.DefaultBranchRef.Name, nil
 }
 

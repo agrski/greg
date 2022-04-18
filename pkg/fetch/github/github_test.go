a4409c069bd75efa14057d42ac1becfd42eae6dd
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2022-04-18 21:49:11 +0100

    Add integration test for default branch refs

--- pkg/fetch/github/github_test.go
+++ pkg/fetch/github/github_test.go
@@ -20,3 +20,38 @@ func getTokenSource(t *testing.T) oauth2.TokenSource {
 
 	return source
 }
+
+func TestGetDefaultBranchRef(t *testing.T) {
+	type test struct {
+		name     string
+		owner    string
+		repo     string
+		expected string
+	}
+
+	tests := []test{
+		{name: "agrski/gitfind", owner: "agrski", repo: "gitfind", expected: "master"},
+		{name: "prometheus/prometheus", owner: "prometheus", repo: "prometheus", expected: "main"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := New(
+				QueryParams{
+					RepoOwner: tt.owner,
+					RepoName:  tt.repo,
+				},
+				getTokenSource(t),
+			)
+
+			name, err := g.getDefaultBranchRef()
+			if err != nil {
+				t.Errorf("failed to query GitHub: %v", err)
+			}
+
+			if tt.expected != name {
+				t.Errorf("expected %s but got %s", tt.expected, name)
+			}
+		})
+	}
+}

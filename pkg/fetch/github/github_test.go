31cf5f761bd3e7518e138ad5dad7b2625559759b
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2022-04-18 21:35:34 +0100

    Add stub for GitHub integration tests with helper func to read access token from test data

---
+++ pkg/fetch/github/github_test.go
@@ -0,0 +1,20 @@
+package github
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/agrski/gitfind/pkg/auth"
+	"golang.org/x/oauth2"
+)
+
+func getTokenSource(t *testing.T) oauth2.TokenSource {
+	tokenPath := filepath.Join("testdata", "token.txt")
+	source, err := auth.TokenSourceFromFile(tokenPath)
+	if os.IsNotExist(err) {
+		t.Errorf("please add a valid GitHub access token to %s", tokenPath)
+	}
+
+	return source
+}

15824e178d4214c814ab073d1e3b508466ac1c61
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2022-04-20 10:50:29 +0100

    Add test for tree-parsing/flattening logic

---
+++ pkg/fetch/github/github_test.go
@@ -0,0 +1,70 @@
+package github
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseTree(t *testing.T) {
+	type treeEntry struct {
+		FileMetadata
+		Object struct {
+			FileContents "graphql:\"... on Blob\""
+		}
+	}
+
+	type test struct {
+		name    string
+		entries []struct {
+			FileMetadata
+			Object struct {
+				FileContents "graphql:\"... on Blob\""
+			}
+		}
+		expectedResults   []*FileInfo
+		expectedRemaining []string
+	}
+
+	tests := []test{
+		{
+			name: "empty root dir",
+			entries: []struct {
+				FileMetadata
+				Object struct {
+					FileContents "graphql:\"... on Blob\""
+				}
+			}{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tree := &treeQuery{}
+			tree.Repository.Name = "some repo"
+			tree.Repository.Object.Tree.Entries = tt.entries
+
+			results := make(chan *FileInfo, 100)
+			remaining := make(chan string, 100)
+			cancel := make(chan struct{}, 1)
+
+			parseTree(tree, results, remaining, cancel)
+
+			close(results)
+			close(remaining)
+
+			actualResults := make([]*FileInfo, 0)
+			for f := range results {
+				actualResults = append(actualResults, f)
+			}
+
+			actualRemaining := make([]string, 0)
+			for r := range remaining {
+				actualRemaining = append(actualRemaining, r)
+			}
+
+			require.ElementsMatch(t, tt.expectedResults, actualResults)
+			require.ElementsMatch(t, tt.expectedRemaining, actualRemaining)
+		})
+	}
+}

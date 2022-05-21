b06052b6ec620745d5bd701eac856c2ab4f8aca4
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2022-05-21 22:16:54 +0100

    Use testify library for tests (#17)
    * Update unit tests to use testify library
    
    * Update go.mod from 'go mod tidy'

--- cmd/cli/args_test.go
+++ cmd/cli/args_test.go
@@ -3,10 +3,10 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/agrski/gitfind/pkg/fetch"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_getFiletypes(t *testing.T) {
@@ -47,9 +47,8 @@ func Test_getFiletypes(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(
 			tt.name, func(t *testing.T) {
-				if got := getFiletypes(tt.filetypes); !reflect.DeepEqual(got, tt.want) {
-					t.Errorf("getFiletypes() = %v, want %v", got, tt.want)
-				}
+				actual := getFiletypes(tt.filetypes)
+				require.ElementsMatch(t, tt.want, actual)
 			},
 		)
 	}
@@ -57,67 +56,68 @@ func Test_getFiletypes(t *testing.T) {
 
 func Test_getLocation(t *testing.T) {
 	type test struct {
-		name string
-		args *rawArgs
-		want fetch.Location
-		err  bool
+		name    string
+		args    *rawArgs
+		want    fetch.Location
+		wantErr bool
 	}
+
 	tests := []test{
 		{
-			name: "fail if any required arg is missing",
-			args: &rawArgs{},
-			want: fetch.Location{},
-			err:  true,
+			name:    "fail if any required arg is missing",
+			args:    &rawArgs{},
+			want:    fetch.Location{},
+			wantErr: true,
 		},
 		{
-			name: "fail if using org without repo",
-			args: &rawArgs{org: "fakeOrg"},
-			want: fetch.Location{},
-			err:  true,
+			name:    "fail if using org without repo",
+			args:    &rawArgs{org: "fakeOrg"},
+			want:    fetch.Location{},
+			wantErr: true,
 		},
 		{
-			name: "fail if using repo without org",
-			args: &rawArgs{repo: "fakeRepo"},
-			want: fetch.Location{},
-			err:  true,
+			name:    "fail if using repo without org",
+			args:    &rawArgs{repo: "fakeRepo"},
+			want:    fetch.Location{},
+			wantErr: true,
 		},
 		{
-			name: "fail if using url with org",
-			args: &rawArgs{url: "https://github.com/fakeOrg/fakeRepo", org: "fakeOrg"},
-			want: fetch.Location{},
-			err:  true,
+			name:    "fail if using url with org",
+			args:    &rawArgs{url: "https://github.com/fakeOrg/fakeRepo", org: "fakeOrg"},
+			want:    fetch.Location{},
+			wantErr: true,
 		},
 		{
-			name: "fail if using url with repo",
-			args: &rawArgs{url: "https://github.com/fakeOrg/fakeRepo", repo: "fakeRepo"},
-			want: fetch.Location{},
-			err:  true,
+			name:    "fail if using url with repo",
+			args:    &rawArgs{url: "https://github.com/fakeOrg/fakeRepo", repo: "fakeRepo"},
+			want:    fetch.Location{},
+			wantErr: true,
 		},
 		{
-			name: "org and repo both provided",
-			args: &rawArgs{host: githubHost, org: "fakeOrg", repo: "fakeRepo"},
-			want: fetch.Location{Host: githubHost, Organisation: "fakeOrg", Repository: "fakeRepo"},
-			err:  false,
+			name:    "org and repo both provided",
+			args:    &rawArgs{host: githubHost, org: "fakeOrg", repo: "fakeRepo"},
+			want:    fetch.Location{Host: githubHost, Organisation: "fakeOrg", Repository: "fakeRepo"},
+			wantErr: false,
 		},
 		{
-			name: "url provided",
-			args: &rawArgs{url: "https://github.com/fakeOrg/fakeRepo"},
-			want: fetch.Location{Host: githubHost, Organisation: "fakeOrg", Repository: "fakeRepo"},
-			err:  false,
+			name:    "url provided",
+			args:    &rawArgs{url: "https://github.com/fakeOrg/fakeRepo"},
+			want:    fetch.Location{Host: githubHost, Organisation: "fakeOrg", Repository: "fakeRepo"},
+			wantErr: false,
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(
 			tt.name, func(t *testing.T) {
-				got, err := getLocation(tt.args)
-				gotErr := err != nil
+				actual, err := getLocation(tt.args)
 
-				if tt.err != gotErr {
-					t.Errorf("getLocation() = %v %v, want %v %v", got, err, tt.want, tt.err)
+				if tt.wantErr {
+					require.Error(t, err)
+				} else {
+					require.NoError(t, err)
 				}
-				if !reflect.DeepEqual(got, tt.want) {
-					t.Errorf("getLocation() = %v %v, want %v %v", got, err, tt.want, tt.err)
-				}
+				require.Equal(t, tt.want, actual)
 			},
 		)
 	}
@@ -128,32 +128,35 @@ func Test_getSearchPattern(t *testing.T) {
 		name          string
 		searchPattern string
 		want          string
-		err           bool
+		wantErr       bool
 	}
+
 	tests := []test{
 		{
 			name:          "should return pattern when provided",
 			searchPattern: "hello",
 			want:          "hello",
-			err:           false,
+			wantErr:       false,
 		},
 		{
 			name:          "should fail when pattern is not provided",
 			searchPattern: "",
 			want:          "",
-			err:           true,
+			wantErr:       true,
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(
 			tt.name, func(t *testing.T) {
-				got, err := getSearchPattern(tt.searchPattern)
-				if tt.err != (err != nil) {
-					t.Errorf("getSearchPattern() = \"'%v', %v\", want \"'%v', %v\"", got, err, tt.want, tt.err)
+				actual, err := getSearchPattern(tt.searchPattern)
+
+				if tt.wantErr {
+					require.Error(t, err)
+				} else {
+					require.NoError(t, err)
 				}
-				if got != tt.want {
-					t.Errorf("getSearchPattern() = \"'%v', %v\", want \"'%v', %v\"", got, err, tt.want, tt.err)
-				}
+				require.Equal(t, tt.want, actual)
 			},
 		)
 	}
@@ -202,9 +205,8 @@ func Test_isEmpty(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(
 			tt.name, func(t *testing.T) {
-				if got := isEmpty(tt.input); got != tt.want {
-					t.Errorf("isEmpty() = %v, want %v", got, tt.want)
-				}
+				actual := isEmpty(tt.input)
+				require.Equal(t, tt.want, actual)
 			},
 		)
 	}
@@ -212,74 +214,74 @@ func Test_isEmpty(t *testing.T) {
 
 func Test_parseLocationFromURL(t *testing.T) {
 	type test struct {
-		name   string
-		rawUrl string
-		want   fetch.Location
-		err    bool
+		name    string
+		rawUrl  string
+		want    fetch.Location
+		wantErr bool
 	}
 
 	tests := []test{
 		{
-			name:   "full URL",
-			rawUrl: "https://github.com/agrski/gitfind",
-			want:   fetch.Location{Host: "github.com", Organisation: "agrski", Repository: "gitfind"},
-			err:    false,
+			name:    "full URL",
+			rawUrl:  "https://github.com/agrski/gitfind",
+			want:    fetch.Location{Host: "github.com", Organisation: "agrski", Repository: "gitfind"},
+			wantErr: false,
 		},
 		{
-			name:   "full URL with git suffix",
-			rawUrl: "https://github.com/agrski/gitfind.git",
-			want:   fetch.Location{Host: "github.com", Organisation: "agrski", Repository: "gitfind"},
-			err:    false,
+			name:    "full URL with git suffix",
+			rawUrl:  "https://github.com/agrski/gitfind.git",
+			want:    fetch.Location{Host: "github.com", Organisation: "agrski", Repository: "gitfind"},
+			wantErr: false,
 		},
 		{
-			name:   "without scheme",
-			rawUrl: "github.com/agrski/gitfind",
-			want:   fetch.Location{Host: "github.com", Organisation: "agrski", Repository: "gitfind"},
-			err:    false,
+			name:    "without scheme",
+			rawUrl:  "github.com/agrski/gitfind",
+			want:    fetch.Location{Host: "github.com", Organisation: "agrski", Repository: "gitfind"},
+			wantErr: false,
 		},
 		{
-			name:   "with extra path",
-			rawUrl: "https://github.com/agrski/gitfind/tree/master/pkg",
-			want:   fetch.Location{Host: "github.com", Organisation: "agrski", Repository: "gitfind"},
-			err:    false,
+			name:    "with extra path",
+			rawUrl:  "https://github.com/agrski/gitfind/tree/master/pkg",
+			want:    fetch.Location{Host: "github.com", Organisation: "agrski", Repository: "gitfind"},
+			wantErr: false,
 		},
 		{
-			name:   "full URL - GitLab",
-			rawUrl: "https://gitlab.com/agrski/gitfind",
-			want:   fetch.Location{Host: "gitlab.com", Organisation: "agrski", Repository: "gitfind"},
-			err:    false,
+			name:    "full URL - GitLab",
+			rawUrl:  "https://gitlab.com/agrski/gitfind",
+			want:    fetch.Location{Host: "gitlab.com", Organisation: "agrski", Repository: "gitfind"},
+			wantErr: false,
 		},
 		{
-			name:   "missing repo - trailing slash",
-			rawUrl: "https://github.com/agrski/",
-			want:   fetch.Location{},
-			err:    true,
+			name:    "missing repo - trailing slash",
+			rawUrl:  "https://github.com/agrski/",
+			want:    fetch.Location{},
+			wantErr: true,
 		},
 		{
-			name:   "missing repo - no trailing slash",
-			rawUrl: "https://github.com/agrski",
-			want:   fetch.Location{},
-			err:    true,
+			name:    "missing repo - no trailing slash",
+			rawUrl:  "https://github.com/agrski",
+			want:    fetch.Location{},
+			wantErr: true,
 		},
 		{
-			name:   "missing org and repo",
-			rawUrl: "https://github.com/",
-			want:   fetch.Location{},
-			err:    true,
+			name:    "missing org and repo",
+			rawUrl:  "https://github.com/",
+			want:    fetch.Location{},
+			wantErr: true,
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(
 			tt.name, func(t *testing.T) {
-				got, err := parseLocationFromURL(tt.rawUrl)
-				gotErr := err != nil
+				actual, err := parseLocationFromURL(tt.rawUrl)
 
-				if tt.err != gotErr {
-					t.Errorf("parseLocationFromURL() = %v %v, want %v %v", got, err, tt.want, tt.err)
+				if tt.wantErr {
+					require.Error(t, err)
+				} else {
+					require.NoError(t, err)
 				}
-				if !reflect.DeepEqual(got, tt.want) {
-					t.Errorf("parseLocationFromURL() = %v %v, want %v %v", got, err, tt.want, tt.err)
-				}
+				require.Equal(t, tt.want, actual)
 			},
 		)
 	}
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.16
 
 require (
 	github.com/hasura/go-graphql-client v0.6.3
-	github.com/rs/zerolog v1.26.1 // indirect
+	github.com/rs/zerolog v1.26.1
 	github.com/stretchr/testify v1.4.0
 	golang.org/x/oauth2 v0.0.0-20220223155221-ee480838109b
 )

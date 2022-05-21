1e4d402a3854b538786839553f4bc1b05f95b9ea
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2022-05-21 14:27:58 +0100

    Move main function to top of file for visibility
    Follow the approach of: entrypoint/public API first, called/dependencies after.

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -10,39 +10,6 @@ import (
 	"github.com/rs/zerolog"
 )
 
-func makeLogger(level zerolog.Level) zerolog.Logger {
-	fieldKeyFormatter := func(v interface{}) string {
-		return strings.ToUpper(
-			fmt.Sprintf("%s=", v),
-		)
-	}
-	logWriter := zerolog.ConsoleWriter{
-		Out:        os.Stderr,
-		TimeFormat: time.RFC3339,
-		FormatLevel: func(v interface{}) string {
-			l, ok := v.(string)
-			switch {
-			case !ok, l == zerolog.InfoLevel.String():
-				return ""
-			default:
-				return strings.ToUpper(
-					fmt.Sprintf("%-6s ", v),
-				)
-			}
-		},
-		FormatFieldName:    fieldKeyFormatter,
-		FormatErrFieldName: fieldKeyFormatter,
-	}
-	logger := zerolog.
-		New(logWriter).
-		Level(level).
-		With().
-		Timestamp().
-		Logger()
-
-	return logger
-}
-
 func main() {
 	logger := makeLogger(zerolog.InfoLevel)
 
@@ -83,4 +50,37 @@ func main() {
 	}
 
 	fetcher.Stop()
+}
+
+func makeLogger(level zerolog.Level) zerolog.Logger {
+	fieldKeyFormatter := func(v interface{}) string {
+		return strings.ToUpper(
+			fmt.Sprintf("%s=", v),
+		)
+	}
+	logWriter := zerolog.ConsoleWriter{
+		Out:        os.Stderr,
+		TimeFormat: time.RFC3339,
+		FormatLevel: func(v interface{}) string {
+			l, ok := v.(string)
+			switch {
+			case !ok, l == zerolog.InfoLevel.String():
+				return ""
+			default:
+				return strings.ToUpper(
+					fmt.Sprintf("%-6s ", v),
+				)
+			}
+		},
+		FormatFieldName:    fieldKeyFormatter,
+		FormatErrFieldName: fieldKeyFormatter,
+	}
+	logger := zerolog.
+		New(logWriter).
+		Level(level).
+		With().
+		Timestamp().
+		Logger()
+
+	return logger
 }

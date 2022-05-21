b01d6b488b11542ff87607f092c719ede64d0189
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2022-05-21 21:43:26 +0100

    Refactor arg handling (#15)
    * Move arg-handling logic to separate file
    
    * Update CLI Make target to not reference single file
    
    * Move main function to top of file for visibility
    
    Follow the approach of: entrypoint/public API first, called/dependencies after.
    
    * Add struct for raw args to replace package-level vars
    
    * Return raw args struct from flag-parsing function
    
    * Update arg parsing functions to use raw args instead of package vars
    
    * Update main function to use arg-parsing entrypoint function
    
    * Remove package-level flag vars
    
    * Pass raw args to access token function for consistency
    
    Other functions also use raw args, so having one not do so is unexpected.
    
    * Update arg-parsing tests to use raw args
    
    * Update arg-handling tests for utilities for consistency
    
    * Remove superfluous fields from args struct
    
    * Return error unless exactly 1 search term is provided
    
    * Refactor hostname validation entirely to function
    
    Move error creation to function.
    Move type casting to function.
    
    * Pass exact field not entire raw args struct to parsing/validation functions
    
    For one or two parameters, it is clearer to pass just those parameters to a function,
    rather than an entire struct; this conveys what information is used.
    For more parameters, this induces unnecessary copying and verbosity.
    
    * Move function from args to alongside caller in main
    
    * Rename test file to reflect move of arg-parsing to separate file
    
    * Move test to separate file to reflect location of function under test
    
    * Update argument types in arg-parsing tests
    
    * Rename arg as plural for semantic clarity

---
+++ cmd/cli/args.go
@@ -0,0 +1,220 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"strings"
+
+	"github.com/agrski/gitfind/pkg/auth"
+	"github.com/agrski/gitfind/pkg/fetch"
+	"golang.org/x/oauth2"
+)
+
+const (
+	httpScheme = "https"
+	githubHost = "github.com"
+)
+
+var supportedHosts = [...]fetch.HostName{githubHost}
+
+type rawArgs struct {
+	host            string
+	org             string
+	repo            string
+	url             string
+	filetypes       string
+	searchPattern   string
+	accessToken     string
+	accessTokenFile string
+}
+
+type Args struct {
+	location      fetch.Location
+	searchPattern string
+	filetypes     []string
+	tokenSource   oauth2.TokenSource
+}
+
+func GetArgs() (*Args, error) {
+	raw, err := parseArguments()
+	if err != nil {
+		return nil, err
+	}
+
+	err = isSupportedHost(raw.host)
+	if err != nil {
+		return nil, err
+	}
+
+	location, err := getLocation(raw)
+	if err != nil {
+		return nil, err
+	}
+
+	pattern, err := getSearchPattern(raw.searchPattern)
+	if err != nil {
+		return nil, err
+	}
+
+	tokenSource, err := getAccessToken(raw.accessToken, raw.accessTokenFile)
+	if err != nil {
+		return nil, err
+	}
+
+	filetypes := getFiletypes(raw.filetypes)
+
+	return &Args{
+		location:      location,
+		searchPattern: pattern,
+		filetypes:     filetypes,
+		tokenSource:   tokenSource,
+	}, nil
+}
+
+func parseArguments() (*rawArgs, error) {
+	args := rawArgs{}
+
+	flag.StringVar(&args.host, "host", githubHost, "git hostname, default: github.com")
+	flag.StringVar(&args.org, "org", "", "organisation name, e.g. agrski")
+	flag.StringVar(&args.repo, "repo", "", "repository name, e.g. gitfind")
+	flag.StringVar(
+		&args.url,
+		"url",
+		"",
+		"Full URL of git repository, e.g https://github.com/agrski/gitfind",
+	)
+	flag.StringVar(&args.filetypes, "type", "", "filetype suffix, e.g. md or go")
+	flag.StringVar(&args.accessToken, "access-token", "", "raw access token for repository access")
+	flag.StringVar(
+		&args.accessTokenFile,
+		"access-token-file",
+		"",
+		"file containing access token for repository access",
+	)
+	flag.Parse()
+
+	if 1 != flag.NArg() {
+		return nil, fmt.Errorf("expected one search term but found %d", flag.NArg())
+	}
+	args.searchPattern = flag.Arg(0)
+
+	return &args, nil
+}
+
+func getLocation(args *rawArgs) (fetch.Location, error) {
+	if isEmpty(args.url) && (isEmpty(args.org) || isEmpty(args.repo)) {
+		return fetch.Location{}, errors.New("must specify either url or both org and repo")
+	}
+
+	if !isEmpty(args.url) && (!isEmpty(args.org) || !isEmpty(args.repo)) {
+		return fetch.Location{}, errors.New("cannot specify both url and org or repo")
+	}
+
+	if isEmpty(args.url) {
+		return fetch.Location{
+			Host:         fetch.HostName(args.host),
+			Organisation: fetch.OrganisationName(args.org),
+			Repository:   fetch.RepositoryName(args.repo),
+		}, nil
+	}
+
+	return parseLocationFromURL(args.url)
+}
+
+func parseLocationFromURL(rawURL string) (fetch.Location, error) {
+	if isEmpty(rawURL) {
+		return fetch.Location{}, errors.New("cannot parse empty string")
+	}
+
+	noWhitespace := strings.TrimSpace(rawURL)
+
+	parts := strings.SplitAfter(noWhitespace, "://")
+	if len(parts) > 2 {
+		return fetch.Location{}, fmt.Errorf("cannot parse malformed string '%v'", noWhitespace)
+	}
+
+	withoutScheme := parts[len(parts)-1]
+	hostAndPath := strings.Split(withoutScheme, "/")
+	if len(hostAndPath) < 3 {
+		return fetch.Location{}, fmt.Errorf("unable to parse host, org, and repo from %v", hostAndPath)
+	}
+
+	host := hostAndPath[0]
+	org := hostAndPath[1]
+	repo := hostAndPath[2]
+	repo = strings.TrimSuffix(repo, ".git")
+
+	if isEmpty(host) {
+		return fetch.Location{}, errors.New("host cannot be empty")
+	}
+	if isEmpty(org) {
+		return fetch.Location{}, errors.New("org cannot be empty")
+	}
+	if isEmpty(repo) {
+		return fetch.Location{}, errors.New("repo cannot be empty")
+	}
+
+	return fetch.Location{
+		Host:         fetch.HostName(host),
+		Organisation: fetch.OrganisationName(org),
+		Repository:   fetch.RepositoryName(repo),
+	}, nil
+}
+
+func getFiletypes(filetypes string) []string {
+	if isEmpty(filetypes) {
+		return nil
+	}
+
+	suffixes := strings.Split(filetypes, ",")
+	for idx, s := range suffixes {
+		withoutWhitespace := strings.TrimSpace(s)
+		withoutLeadingDot := strings.TrimPrefix(withoutWhitespace, ".")
+
+		suffixes[idx] = withoutLeadingDot
+	}
+
+	return suffixes
+}
+
+func getSearchPattern(pattern string) (string, error) {
+	if isEmpty(pattern) {
+		return "", errors.New("search term must be specified; wrap multiple words in quotes")
+	}
+	return pattern, nil
+}
+
+func isEmpty(s string) bool {
+	return "" == strings.TrimSpace(s)
+}
+
+func isSupportedHost(host string) error {
+	hostname := fetch.HostName(host)
+	for _, h := range supportedHosts {
+		if hostname == h {
+			return nil
+		}
+	}
+	return fmt.Errorf("unsupported git hosting provider %s", host)
+}
+
+func getAccessToken(
+	accessToken string,
+	accessTokenFile string,
+) (oauth2.TokenSource, error) {
+	if isEmpty(accessToken) && isEmpty(accessTokenFile) {
+		return nil, errors.New("must specify either access token or access token file")
+	}
+
+	if !isEmpty(accessToken) && !isEmpty(accessTokenFile) {
+		return nil, errors.New("only one of access token and access token file may be specified")
+	}
+
+	tokenSource, err := auth.TokenSourceFromString(accessToken)
+	if err != nil {
+		tokenSource, err = auth.TokenSourceFromFile(accessTokenFile)
+	}
+
+	return tokenSource, err
+}
---
+++ cmd/cli/args_test.go
@@ -0,0 +1,286 @@
+//go:build !integration
+
+package main
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/agrski/gitfind/pkg/fetch"
+)
+
+func Test_getFiletypes(t *testing.T) {
+	type test struct {
+		name      string
+		filetypes string
+		want      []string
+	}
+
+	tests := []test{
+		{
+			name:      "no filetypes succeeds with nil",
+			filetypes: "",
+			want:      nil,
+		},
+		{
+			name:      "simple letter-only filetype is unchanged",
+			filetypes: "md",
+			want:      []string{"md"},
+		},
+		{
+			name:      "dot prefix is removed",
+			filetypes: ".md",
+			want:      []string{"md"},
+		},
+		{
+			name:      "multiple suffices are handled correctly",
+			filetypes: ".md,go,.txt",
+			want:      []string{"md", "go", "txt"},
+		},
+		{
+			name:      "multiple suffices with whitespace are handled correctly",
+			filetypes: ".md,go , .txt",
+			want:      []string{"md", "go", "txt"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				if got := getFiletypes(tt.filetypes); !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("getFiletypes() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
+
+func Test_getLocation(t *testing.T) {
+	type test struct {
+		name string
+		args *rawArgs
+		want fetch.Location
+		err  bool
+	}
+	tests := []test{
+		{
+			name: "fail if any required arg is missing",
+			args: &rawArgs{},
+			want: fetch.Location{},
+			err:  true,
+		},
+		{
+			name: "fail if using org without repo",
+			args: &rawArgs{org: "fakeOrg"},
+			want: fetch.Location{},
+			err:  true,
+		},
+		{
+			name: "fail if using repo without org",
+			args: &rawArgs{repo: "fakeRepo"},
+			want: fetch.Location{},
+			err:  true,
+		},
+		{
+			name: "fail if using url with org",
+			args: &rawArgs{url: "https://github.com/fakeOrg/fakeRepo", org: "fakeOrg"},
+			want: fetch.Location{},
+			err:  true,
+		},
+		{
+			name: "fail if using url with repo",
+			args: &rawArgs{url: "https://github.com/fakeOrg/fakeRepo", repo: "fakeRepo"},
+			want: fetch.Location{},
+			err:  true,
+		},
+		{
+			name: "org and repo both provided",
+			args: &rawArgs{host: githubHost, org: "fakeOrg", repo: "fakeRepo"},
+			want: fetch.Location{Host: githubHost, Organisation: "fakeOrg", Repository: "fakeRepo"},
+			err:  false,
+		},
+		{
+			name: "url provided",
+			args: &rawArgs{url: "https://github.com/fakeOrg/fakeRepo"},
+			want: fetch.Location{Host: githubHost, Organisation: "fakeOrg", Repository: "fakeRepo"},
+			err:  false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				got, err := getLocation(tt.args)
+				gotErr := err != nil
+
+				if tt.err != gotErr {
+					t.Errorf("getLocation() = %v %v, want %v %v", got, err, tt.want, tt.err)
+				}
+				if !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("getLocation() = %v %v, want %v %v", got, err, tt.want, tt.err)
+				}
+			},
+		)
+	}
+}
+
+func Test_getSearchPattern(t *testing.T) {
+	type test struct {
+		name          string
+		searchPattern string
+		want          string
+		err           bool
+	}
+	tests := []test{
+		{
+			name:          "should return pattern when provided",
+			searchPattern: "hello",
+			want:          "hello",
+			err:           false,
+		},
+		{
+			name:          "should fail when pattern is not provided",
+			searchPattern: "",
+			want:          "",
+			err:           true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				got, err := getSearchPattern(tt.searchPattern)
+				if tt.err != (err != nil) {
+					t.Errorf("getSearchPattern() = \"'%v', %v\", want \"'%v', %v\"", got, err, tt.want, tt.err)
+				}
+				if got != tt.want {
+					t.Errorf("getSearchPattern() = \"'%v', %v\", want \"'%v', %v\"", got, err, tt.want, tt.err)
+				}
+			},
+		)
+	}
+}
+
+func Test_isEmpty(t *testing.T) {
+	type test struct {
+		name  string
+		input string
+		want  bool
+	}
+
+	tests := []test{
+		{
+			name:  "empty-string should be empty",
+			input: "",
+			want:  true,
+		},
+		{
+			name:  "spaces should be empty",
+			input: "   ",
+			want:  true,
+		},
+		{
+			name:  "tabs should be empty",
+			input: "\t\t",
+			want:  true,
+		},
+		{
+			name:  "mixed whitespace should be empty",
+			input: " \t \u00a0 \u2003",
+			want:  true,
+		},
+		{
+			name:  "word should not be empty",
+			input: "hello",
+			want:  false,
+		},
+		{
+			name:  "multiple words should not be empty",
+			input: "hello world",
+			want:  false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				if got := isEmpty(tt.input); got != tt.want {
+					t.Errorf("isEmpty() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
+
+func Test_parseLocationFromURL(t *testing.T) {
+	type test struct {
+		name   string
+		rawUrl string
+		want   fetch.Location
+		err    bool
+	}
+
+	tests := []test{
+		{
+			name:   "full URL",
+			rawUrl: "https://github.com/agrski/gitfind",
+			want:   fetch.Location{Host: "github.com", Organisation: "agrski", Repository: "gitfind"},
+			err:    false,
+		},
+		{
+			name:   "full URL with git suffix",
+			rawUrl: "https://github.com/agrski/gitfind.git",
+			want:   fetch.Location{Host: "github.com", Organisation: "agrski", Repository: "gitfind"},
+			err:    false,
+		},
+		{
+			name:   "without scheme",
+			rawUrl: "github.com/agrski/gitfind",
+			want:   fetch.Location{Host: "github.com", Organisation: "agrski", Repository: "gitfind"},
+			err:    false,
+		},
+		{
+			name:   "with extra path",
+			rawUrl: "https://github.com/agrski/gitfind/tree/master/pkg",
+			want:   fetch.Location{Host: "github.com", Organisation: "agrski", Repository: "gitfind"},
+			err:    false,
+		},
+		{
+			name:   "full URL - GitLab",
+			rawUrl: "https://gitlab.com/agrski/gitfind",
+			want:   fetch.Location{Host: "gitlab.com", Organisation: "agrski", Repository: "gitfind"},
+			err:    false,
+		},
+		{
+			name:   "missing repo - trailing slash",
+			rawUrl: "https://github.com/agrski/",
+			want:   fetch.Location{},
+			err:    true,
+		},
+		{
+			name:   "missing repo - no trailing slash",
+			rawUrl: "https://github.com/agrski",
+			want:   fetch.Location{},
+			err:    true,
+		},
+		{
+			name:   "missing org and repo",
+			rawUrl: "https://github.com/",
+			want:   fetch.Location{},
+			err:    true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				got, err := parseLocationFromURL(tt.rawUrl)
+				gotErr := err != nil
+
+				if tt.err != gotErr {
+					t.Errorf("parseLocationFromURL() = %v %v, want %v %v", got, err, tt.want, tt.err)
+				}
+				if !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("parseLocationFromURL() = %v %v, want %v %v", got, err, tt.want, tt.err)
+				}
+			},
+		)
+	}
+}

e96cca8e0d8b13d99c7cea73c52118885d99deab
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2022-05-22 17:00:21 +0100

    Add interface & return value type for matcher

--- pkg/match/match.go
+++ pkg/match/match.go
@@ -1,1 +1,13 @@
 package match
+
+import (
+	"github.com/agrski/greg/pkg/fetch/github"
+)
+
+type Matcher interface {
+	Match(pattern string, next *github.FileInfo) (*Match, bool)
+}
+
+type Match struct {
+	line uint
+}

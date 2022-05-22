cf808f76317c32484808a84d72eefd9217ddebc1
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2022-05-22 23:46:03 +0100

    Add test case for file-type filtering

--- pkg/match/filetype_test.go
+++ pkg/match/filetype_test.go
@@ -3,6 +3,7 @@ package match
 import (
 	"testing"
 
+	"github.com/agrski/greg/pkg/fetch/github"
 	"github.com/stretchr/testify/require"
 )
 
@@ -44,6 +45,35 @@ func TestNormaliseExtension(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			actual := NormaliseExtension(tt.extension)
+			require.Equal(t, tt.expected, actual)
+		})
+	}
+}
+
+func TestFilterFiletype(t *testing.T) {
+	type test struct {
+		name      string
+		allowed   []string
+		extension string
+		expected  bool
+	}
+
+	tests := []test{
+		{
+			name:      "should allow everything when allowed is nil",
+			allowed:   nil,
+			extension: "md",
+			expected:  true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fileInfo := &github.FileInfo{}
+			fileInfo.Extension = tt.extension
+
+			actual := FilterFiletype(tt.allowed, fileInfo)
+
 			require.Equal(t, tt.expected, actual)
 		})
 	}

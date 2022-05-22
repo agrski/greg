12a5b6b71691cd781fb17cc79bd4c4d503d41bb2
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2022-05-22 20:06:22 +0100

    Add file-type filtering matcher as decorator for content-filtering matchers

--- pkg/match/match.go
+++ pkg/match/match.go
@@ -2,6 +2,7 @@ package match
 
 import (
 	"github.com/agrski/greg/pkg/fetch/github"
+	"github.com/rs/zerolog"
 )
 
 type Matcher interface {
@@ -12,4 +13,30 @@ type Matcher interface {
 
 type Match struct {
 	line uint
+}
+
+type FilteringMatcher struct {
+	matcher   Matcher
+	filetypes []string
+	logger    zerolog.Logger
+}
+
+var _ Matcher = (*FilteringMatcher)(nil)
+
+func New(logger zerolog.Logger, allowedFiletypes []string) *FilteringMatcher {
+	em := newExactMatcher(logger)
+	logger = logger.With().Str("source", "FilteringMatcher").Logger()
+
+	return &FilteringMatcher{
+		matcher:   em,
+		filetypes: allowedFiletypes,
+		logger:    logger,
+	}
+}
+
+func (fm *FilteringMatcher) Match(pattern string, next *github.FileInfo) (*Match, bool) {
+	if ok := FilterFiletype(fm.filetypes, next); !ok {
+		return nil, false
+	}
+	return fm.matcher.Match(pattern, next)
 }

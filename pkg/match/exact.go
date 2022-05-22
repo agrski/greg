f3a3d4f39ac1059e2c16c68d26c575eff1ba343c
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2022-05-23 00:03:47 +0100

    Implement matching algorithm for exact matcher

--- pkg/match/exact.go
+++ pkg/match/exact.go
@@ -1,6 +1,9 @@
 package match
 
 import (
+	"bufio"
+	"strings"
+
 	"github.com/agrski/greg/pkg/fetch/github"
 	"github.com/rs/zerolog"
 )
@@ -19,5 +22,32 @@ func newExactMatcher(logger zerolog.Logger) *exactMatcher {
 }
 
 func (em *exactMatcher) Match(pattern string, next *github.FileInfo) (*Match, bool) {
-	return nil, false
+	if next.IsBinary {
+		return nil, false
+	}
+
+	match := &Match{
+		lines: []FilePosition{},
+	}
+	lineReader := bufio.NewScanner(
+		strings.NewReader(next.Text),
+	)
+	row := uint(0)
+
+	for lineReader.Scan() {
+		row++
+		column := strings.Index(next.Text, pattern)
+		if column >= 0 {
+			match.lines = append(
+				match.lines,
+				FilePosition{line: row, column: uint(column)},
+			)
+		}
+	}
+
+	if err := lineReader.Err(); err != nil {
+		return nil, false
+	}
+
+	return match, true
 }

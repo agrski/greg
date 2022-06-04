ee3751fbf654313665dec9872c4f96d7b30d628e
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2022-06-04 19:30:04 +0100

    Add missing path fields in GitHub unit tests

--- pkg/fetch/github/github_test.go
+++ pkg/fetch/github/github_test.go
@@ -56,6 +56,7 @@ func TestParseTree(t *testing.T) {
 					fileMetadata{
 						Type:      TreeEntryFile,
 						Name:      "file1.txt",
+						Path:      "file1.txt",
 						Extension: ".txt",
 					},
 					entryObject{
@@ -83,6 +84,7 @@ func TestParseTree(t *testing.T) {
 					fileMetadata{
 						Type:      TreeEntryFile,
 						Name:      "file1.txt",
+						Path:      "file1.txt",
 						Extension: ".txt",
 					},
 					entryObject{

492b7ac3b999a711157efffdeca03a23f97c7608
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2022-06-05 16:37:08 +0100

    Add basic console writer for matches

---
+++ pkg/present/console/console.go
@@ -0,0 +1,46 @@
+package console
+
+import (
+	"strings"
+
+	"github.com/rs/zerolog"
+
+	"github.com/agrski/greg/pkg/match"
+	"github.com/agrski/greg/pkg/types"
+)
+
+type Console struct {
+	enableColour bool
+	logger       zerolog.Logger
+}
+
+func New(logger zerolog.Logger, enableColour bool) *Console {
+	return &Console{
+		enableColour: enableColour,
+		logger:       logger,
+	}
+}
+
+func (c *Console) Write(fileInfo *types.FileInfo, match *match.Match) {
+	c.logger.Log().Msg(string(FgBlue) + fileInfo.Path + string(Reset))
+
+	for _, p := range match.Positions {
+		sb := strings.Builder{}
+
+		// Line number
+		sb.WriteString(string(FgMagenta))
+		sb.WriteString(fileInfo.Path)
+		sb.WriteString(string(Reset))
+		sb.WriteByte(':')
+		// Text
+		sb.WriteString(p.Text[:p.ColumnStart])
+		sb.WriteString(string(FgRed))
+		sb.WriteString(p.Text[p.ColumnStart : p.ColumnStart+p.Length])
+		sb.WriteString(string(Reset))
+		sb.WriteString(p.Text[p.ColumnStart+p.Length:])
+
+		c.logger.Log().Msg(sb.String())
+	}
+
+	c.logger.Log().Send()
+}
